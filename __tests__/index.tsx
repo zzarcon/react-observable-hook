7cd1edabe66a8e54a54b0f664010b51b81385d6f
Dean Radcliffe <deanmisc@yahoo.com>
2018-12-17 09:09:49 -0600

    feat: return cancel function

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,5 +1,29 @@
-describe('useObservable()', () => {
-  it('Foo', () => {
-    expect(1).toEqual(1);
+import { useObservable } from "../src/";
+import React from "react";
+
+import { timer } from "rxjs";
+import {  } from "rxjs/operators";
+import { render } from 'react-testing-library'
+
+const seconds5 = () => timer(5000);
+
+const Hello = () => <b>Hello</b>
+const Timer = () => {
+  const [result, cancel] = useObservable<Object>(seconds5());
+  const { total, elapsed, remaining } = result;
+  return (
+    <div>
+      Total: {total}
+    </div>
+  );
+};
+
+describe("Smoke Test", () => {
+  expect(render(<Hello/>)).not.toThrow();
+})
+describe("useObservable()", () => {
+  it("Timer example", () => {
+    expect(render(<Timer/>)).not.toThrow();
+    // TODO elaborate once testing is working
   });
 });
--- src/useObservable.tsx
+++ src/useObservable.tsx
@@ -1,10 +1,11 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { Observable } from 'rxjs';
 
 // TODO: make getInitialState?: T | () => T
-export function useObservable<T>(observable: Observable<T>, getInitialState?: () => T): T | undefined {
+export function useObservable<T>(observable: Observable<T>, getInitialState?: () => T): [T, Function] | undefined {
   const [value, setValue] = useState(getInitialState && getInitialState());
 
+  let cancel
   useEffect(() => {
     const subscription = observable.subscribe({
       next(value) {
@@ -12,9 +13,9 @@ export function useObservable<T>(observable: Observable<T>, getInitialState?: ()
       }
     });
 
-    const cancel = () => subscription.unsubscribe();
+    cancel = () => subscription.unsubscribe();
     return cancel;
   }, [observable])
-  
+
   return [value, cancel];
 }
